b0be02d8de5de2b5145062ef0b93dcfcf30efc75
Tibor Bodecs <mail.tib@gmail.com>
2022-03-07 09:38:42 +0100

    separated api

--- Package.swift
+++ Package.swift
@@ -8,37 +8,20 @@ let package = Package(
     ],
     products: [
         .library(name: "WebModule", targets: ["WebModule"]),
-        .library(name: "WebApi", targets: ["WebApi"]),
     ],
     dependencies: [
         .package(url: "https://github.com/feathercms/feather-core", .branch("dev")),
         .package(url: "https://github.com/feathercms/feather-api", .branch("main")),
-        .package(url: "https://github.com/vapor/fluent-sqlite-driver", from: "4.0.0"),
-        .package(url: "https://github.com/binarybirds/liquid-local-driver", from: "1.3.0"),
-        .package(url: "https://github.com/binarybirds/swift-html", from: "1.6.0"),
+        .package(url: "https://github.com/feathercms/web-api", .branch("main")),
     ],
     targets: [
-        
-        .executableTarget(name: "WebApp", dependencies: [
-            .target(name: "WebModule"),
-            
-            .product(name: "Feather", package: "feather-core"),
-            
-            .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
-            .product(name: "LiquidLocalDriver", package: "liquid-local-driver"),
-        ]),
         .target(name: "WebModule", dependencies: [
-                .target(name: "WebApi"),
                 .product(name: "FeatherApi", package: "feather-api"),
+                .product(name: "WebApi", package: "web-api"),
                 .product(name: "Feather", package: "feather-core"),
-                .product(name: "SwiftRss", package: "swift-html"),
-                .product(name: "SwiftSitemap", package: "swift-html"),
             ],
             resources: [
                 .copy("Bundle"),
             ]),
-        .target(name: "WebApi", dependencies: [
-            .product(name: "FeatherApi", package: "feather-api"),
-        ]),
     ]
 )
