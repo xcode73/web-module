dfc401d926818d12b4096db183e157c4ad6f72cf
xcode73 <xcode.uln@gmail.com>
2024-02-20 16:17:19 +0400

    change to macos10

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "web-module",
     platforms: [
-       .macOS(.v12)
+        .macOS(.v10_15)
     ],
     products: [
         .library(name: "WebModule", targets: ["WebModule"]),
