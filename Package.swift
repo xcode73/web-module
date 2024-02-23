75d05342e38fe1f982aaf318a8f117a0996b2bfb
xcode73 <xcode.uln@gmail.com>
2024-02-23 16:53:59 +0400

    swift-tools-version:5.9

--- Package.swift
+++ Package.swift
@@ -1,25 +1,26 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "web-module",
     platforms: [
-        .macOS(.v10_15),
+        .macOS(.v10_15)
     ],
     products: [
-        .library(name: "WebModule", targets: ["WebModule"]),
+        .library(name: "WebModule", targets: ["WebModule"])
     ],
     dependencies: [
-        .package(url: "https://github.com/xcode73/feather-core", .branch("test-dev")),
-        .package(url: "https://github.com/xcode73/web-objects", .branch("test-dev")),
+        .package(url: "https://github.com/xcode73/feather-core", branch: "test-dev"),
+        .package(url: "https://github.com/xcode73/web-objects", branch: "test-dev")
     ],
     targets: [
         .target(name: "WebModule", dependencies: [
                 .product(name: "Feather", package: "feather-core"),
-                .product(name: "WebObjects", package: "web-objects"),
+                .product(name: "WebObjects", package: "web-objects")
             ],
             resources: [
-                .copy("Bundle"),
+                .copy("Bundle")
             ]),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
